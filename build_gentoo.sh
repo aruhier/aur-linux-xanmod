0ac361bf932f626f92f970f405f99a98d9f0853c
Anthony Ruhier <aruhier@mailbox.org>
2025-12-10 00:36:13 +0100

    6.17

--- build_gentoo.sh
+++ build_gentoo.sh
@@ -3,7 +3,7 @@
 sudo rm /usr/src/linux/.config
 
 # CLANG and LTO
-sudo CC=clang CXX=clang++ --preserve-env=PATH -- bash -c 'genkernel --kernel-config=config --nconfig --no-install kernel && cd /usr/src/linux; make modules_install -s -j && make install; rm /var/tmp/genkernel/*'
+sudo CC=clang CXX=clang++ --preserve-env=PATH -- bash -c 'genkernel --kernel-config=config --nconfig --no-install kernel && cd /usr/src/linux; make modules_install -s -j$(nproc) INSTALL_MOD_STRIP=1 && make install && emerge @module-rebuild; rm -rf /var/tmp/genkernel/*'
 
 # GCC
 # sudo --preserve-env=PATH -- bash -c 'genkernel --kernel-config=config --nconfig --no-install kernel && cd /usr/src/linux; make modules_install -s -j && make install; rm /var/tmp/genkernel/*'
--- gen_config.sh
+++ gen_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TAG="6.13"
+TAG="6.17"
 
 cp /var/db/repos/localrepo/sys-kernel/cachyos-sources/files/$TAG/config-bore config
 sed -r -f config.sed -i config
